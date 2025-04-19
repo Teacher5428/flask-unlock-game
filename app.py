bf2e4bbede3f8e71734d9bcb15d1b00abc62f55f
hoamy <hoamy3355@gmail.com>
2025-04-19 16:00:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os  # 用來讀取環境變數
 
 app = Flask(__name__)
 
@@ -17,4 +18,7 @@ def home():
     return render_template("index.html", message=message)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    # 獲取環境變數中的端口，若沒有則使用預設端口 5000
+    port = int(os.environ.get("PORT", 5000))
+    # 讓 Flask 應用在 0.0.0.0 上運行並使用動態端口
+    app.run(host="0.0.0.0", port=port)
